0fc417d9b42fa467673c90c4e69e66df9253048e
hhaakkimm <hhaakkimm@gmail.com>
2021-09-21 02:38:31 +0600

    feat: add burger menu, footer, aside, article

---
+++ src/script.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector(".nav-container");
+
+if (nav) {
+  const toggle = nav.querySelector(".nav-toggle");
+  
+  if (toggle) {
+    toggle.addEventListener("click", () => {
+      if (nav.classList.contains("is-active")) {
+        nav.classList.remove("is-active");
+      }
+      else {
+        nav.classList.add("is-active");
+      }
+    });
+    
+    nav.addEventListener("blur", () => {
+      nav.classList.remove("is-active");
+    });
+  }
+}
